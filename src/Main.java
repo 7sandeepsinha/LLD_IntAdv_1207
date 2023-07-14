d53db7b8b576b3967fdbbb68a9077051f4ea20fc
Sandeep Mahapatra <smahapatra@atlassian.com>
2023-07-14 10:19:25 +0530

    Access Modifiers

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import school.Student;
+
+public class Main extends Student{ // Inheritance
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Student s = new Student();
+        s.rank = 20;
+        System.out.println(s.rank);
+        // s.marks = 95; -> marks is default, so not accessible outside the same package
+    }
+
+    public void demoMethod(){
+        Main main = new Main();
+        main.name = "Student name";
+        Student s = new Student();
+        // s.name = "name";
+        // main.address = ""; -> even after inheritance, private is not accessible
+    }
+}
---
+++ src/work/Office.java
@@ -0,0 +1,8 @@
+package work;
+
+public class Office {
+    public String companyName;
+    private int numberOfEmployees;
+    int revenue;
+    protected int profit;
+}
